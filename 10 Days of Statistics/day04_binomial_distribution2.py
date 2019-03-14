be38eaab37592e5dc2b7f9d5bb530e9c37eb6e7a
Juntao Dong <juntaodong@gmail.com>
2019-03-14 13:10:27 -0400

    update

--- 10 Days of Statistics/day04_binomial_distribution2.py
+++ 10 Days of Statistics/day04_binomial_distribution2.py
@@ -2,12 +2,8 @@
 import math
 def binomial(x, n, p):
     comb = math.factorial(n)/(math.factorial(x)*math.factorial(n-x))
-    return comb*(p**x)*((1-p)**(n-x))
+    return comb*((p/100)**x)*((1-(p/100))**(n-x))
 
-p, n = map(float, input().strip().split())
-p = p/100
-prob = 0
-for i in range(2):
-    prob += binomial(i, n, p)
-print(round(prob, 3))
-print(1-round(prob, 3))
+p, n = map(int, input().strip().split())
+print(round(sum(binomial(x,n,p) for x in range(3)), 3))
+print(round(sum(binomial(x,n,p) for x in range(2,n+1)), 3))
